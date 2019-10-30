062a7d9c5071f6b215ec835e8721e1628698edd3
HTY62006 <38950228+HTY62006@users.noreply.github.com>
2019-10-30 18:13:31 +0800

    Update heap.py

--- Week6/heap.py
+++ Week6/heap.py
@@ -39,3 +39,57 @@
 # [3, 7, 1, 1, 2, 8]
 # [3, 7, 1, 1, 2, 8]
 # -----------------------------------------------
+
+# Max Heap
+def build_maxheap(A):
+    for i in range(len(A)-1, -1, -1):
+        root = i//2
+        maxheapify(A, root)
+
+def maxheapify(A, root):
+    left = root*2+1
+    right = root*2+2
+    if (left < len(A)) and (A[left] > A[root]):
+        MaxIndex = left
+    else:
+        MaxIndex = root
+            
+    if (right < len(A)) and (A[right] > A[MaxIndex]):
+        MaxIndex = right
+            
+    if MaxIndex != root:
+        change = A[root]
+        A[root] = A[MaxIndex]
+        A[MaxIndex] = change
+        maxheapify(A,MaxIndex)
+         
+Array = [3,2,1,1,7,8]
+build_maxheap(Array)
+# [8, 7, 3, 1, 2, 1]
+
+# Min Heap
+def build_minheap(A):
+    for i in range(len(A)-1, -1, -1):
+        root = i//2
+        minheapify(A, root)
+
+def minheapify(A, root):
+    left = root*2+1
+    right = root*2+2
+    if (left < len(A)) and (A[left] < A[root]):
+        minIndex = left
+    else:
+        minIndex = root
+            
+    if (right < len(A)) and (A[right] < A[minIndex]):
+        minIndex = right
+            
+    if MaxIndex != root:
+        change = A[root]
+        A[root] = A[minIndex]
+        A[minIndex] = change
+        minheapify(A,minIndex)
+         
+Array = [3,2,1,1,7,8]
+build_maxheap(Array)
+# [1, 2, 1, 3, 7, 8]
