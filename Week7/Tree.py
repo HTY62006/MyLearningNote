8428a7b060ac25bb1fcb18d94c137fdfa08754a2
HTY62006 <38950228+HTY62006@users.noreply.github.com>
2019-11-01 11:49:33 +0800

    Update Tree.py

--- Week7/Tree.py
+++ Week7/Tree.py
@@ -4,3 +4,20 @@ class Node:
         # left、right類似.next
         self.left = None
         self.right = None
+    # 插入
+    def append(self, value):
+        # 原本寫if value < self.data and self.left == None:，但因為還要考慮right的部分，不能寫在一起(為了要用else if)
+        if value < self.data:
+            if self.left == None:
+                self.left=Node(value) # 因為每個都是節點，節點下方可能也可有節點，所以要創建Node存入left(原先Node的左下方)
+            else:
+                self.left.append(value) # 如果left不為空，直接插入?
+        elif value > self.data:
+            if self.right == None:
+                self.right=Node(value)
+            else:
+                self.right.append(value)
+                
+        
+                
+            
