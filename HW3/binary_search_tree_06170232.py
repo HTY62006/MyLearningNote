d3eb5bd8b7b60e9195d80279592969b4482841d8
HTY62006 <38950228+HTY62006@users.noreply.github.com>
2019-11-22 18:45:01 +0800

    Update binary_search_tree_06170232.py

--- HW3/binary_search_tree_06170232.py
+++ HW3/binary_search_tree_06170232.py
@@ -1,1 +1,7 @@
 
+# 參考資料：
+#[Deleting a node from a BST --- Part 1 (easy cases)](http://www.mathcs.emory.edu/~cheung/Courses/171/Syllabus/9-BinTree/BST-delete.html)
+#[Traversing/searching in a BST](http://www.mathcs.emory.edu/~cheung/Courses/171/Syllabus/9-BinTree/BST-search.html)
+#[Binary Tree and Binary Search Tree in Data Structure](https://www.youtube.com/watch?v=7vw2iIdqHlM&feature=emb_title)
+#[[資料結構] 二元搜尋樹 (Binary Search Tree)](https://ithelp.ithome.com.tw/articles/10205875)
+#演算法圖鑑p.47~p.52
