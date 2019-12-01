b2c9158b5eef1d05c424cc24925212883c78f09e
HTY62006 <38950228+HTY62006@users.noreply.github.com>
2019-12-01 13:19:05 +0800

    Create hash_table_06170232.py

---
+++ HW4/hash_table_06170232.py
@@ -0,0 +1,89 @@
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        """
+        :type val: int
+        :type next: ListNode
+        :rtype: None        
+        """
+class MyHashSet:
+    def __init__(self, capacity=5):
+        self.capacity = capacity
+        self.data = [None] * capacity
+        """
+        :type capacity: int
+        :rtype: None
+        """
+    def add(self, key):
+        """
+        :type key: str
+        :rtype: None
+        """
+        # 要用MD5加密儲存資料
+        h = MD5.new()
+        h.update(key.encode("utf-8"))
+        h = h.hexdigest()
+        # 採16進位
+        bucket = int(h , 16)%self.capacity
+        if self.data[bucket] == None:
+            self.data[bucket] = ListNode(h)
+        else:
+            new = ListNode(h)
+            now = self.data[bucket]
+            while now.next != None:
+                now = now.next
+            now.next = new
+
+    def remove(self, key):
+        """
+        :type key: str
+        :rtype: None
+        """
+        h = MD5.new()
+        h.update(key.encode("utf-8"))
+        h = h.hexdigest()
+        bucket = int(h , 16)%self.capacity
+        del_bucket = self.data[bucket]
+        prev = del_bucket
+        if del_bucket != None:
+            if del_bucket.next != None:
+                if del_bucket.val == h:
+                    self.data[bucket] = del_bucket.next
+                else:
+                    prev = del_bucket
+                    while del_bucket.next:
+                        if del_bucket == h:
+                            prev.next = del_bucket.next
+                        else:
+                            prev = del_bucket
+                            del_bucket = del_bucket.next
+            else:
+                if del_bucket.val == h:
+                    self.data[bucket] = None
+        # 相同質移除
+        if self.contains(key) == True:
+            self.remove(key)
+
+    def contains(self, key):
+        """
+        :type key: str
+        :rtype: bool(True or False)
+        """
+        h = MD5.new()
+        h.update(key.encode("utf-8"))
+        h = h.hexdigest()
+        bucket = int(h , 16)%self.capacity
+        if self.data[bucket] != None:
+            node = self.data[bucket]
+            while node.next != None:
+                node = node.next
+                if node.val == h:
+                    break
+#                 print(node.val)
+            if node.val == h:
+                return True
+            else:
+                return False
+        else:
+            return False
