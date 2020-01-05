9b85b88992dbd580b5f2376ef3c24fbcf848fe71
HTY62006 <38950228+HTY62006@users.noreply.github.com>
2020-01-06 01:06:13 +0800

    Create reverseInParentheses.py

---
+++ Codesignal/Intro/reverseInParentheses.py
@@ -0,0 +1,9 @@
+def reverseInParentheses(inputString):
+    for i in range(len(inputString)):
+        if inputString[i] == "(":
+            start = i
+        elif inputString[i] == ")":
+            end = i
+            return reverseInParentheses(inputString[:start] + inputString[start+1:end][::-1] + inputString[end+1:])
+    return inputString
+    
